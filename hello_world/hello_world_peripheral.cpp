7fdcceeff22e3df662648d617a54acd0350922db
Troy Stribling <troy.stribling@gmail.com>
2014-01-16 22:02:25 -0800

    adding greeting

--- hello_world/hello_world_peripheral.cpp
+++ hello_world/hello_world_peripheral.cpp
@@ -1,3 +1,4 @@
+#include <avr/pgmspace.h>
 #include "hello_world_peripheral.h"
 #include "dlog.h"
 #include "services.h"
@@ -8,6 +9,39 @@
 
 static services_pipe_type_mapping_t services_pipe_type_mapping[NUMBER_OF_PIPES] = SERVICES_PIPE_TYPE_MAPPING_CONTENT;
 static hal_aci_data_t setup_msgs[NB_SETUP_MESSAGES] PROGMEM = SETUP_MESSAGES_CONTENT;
+
+const static  prog_char english[]   PROGMEM   = "Hello";
+const static  prog_char hawaiian[]  PROGMEM   = "Aloha";
+const static  prog_char saxon[]     PROGMEM   = "Moin";
+const static  prog_char latvian[]   PROGMEM   = "Sveiki";
+const static  prog_char norwegian[] PROGMEM   = "Moin";
+const static  prog_char romainian[] PROGMEM   = "Salut";
+const static  prog_char spanish[]   PROGMEM   = "Hola";
+const static  prog_char swedish[]   PROGMEM   = "Hej";
+const static  prog_char italian[]   PROGMEM   = "Ciao";
+const static  prog_char kligon[]    PROGMEM   = "nuqneH";
+const static  prog_char french[]    PROGMEM   = "Bonjour";
+const static  prog_char danish[]    PROGMEM   = "Hej";
+const static  prog_char bosnian[]   PROGMEM   = "Molim";
+
+PROGMEM const char* const greetings[] = {
+  english,
+  hawaiian,
+  saxon,
+  latvian,
+  norwegian,
+  romainian,
+  spanish,
+  swedish,
+  italian,
+  kligon,
+  french,
+  danish,
+  bosnian
+};
+
+static unsigned char count;
+static unsigned char greeting[10];
 
 HelloWorldPeripheral::HelloWorldPeripheral(uint8_t reqn, uint8_t rdyn): BlueCapPeripheral(reqn, rdyn) {
   setServicePipeTypeMapping(services_pipe_type_mapping, NUMBER_OF_PIPES);
@@ -38,6 +72,11 @@ void HelloWorldPeripheral::didReceiveData(uint8_t characteristicId, uint8_t* dat
   }
 }
 
+void HelloWorldPeripheral::didReceiveError(uint8_t pipe, uint8_t) {
+}
+
 void HelloWorldPeripheral::loop() {
+  if (millis() % 5000 == 0) {
+  }
   BlueCapPeripheral::loop();
 }
