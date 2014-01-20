14de7e24a0dd61da2f39db8afde1f5665201b84b
Troy Stribling <troy.stribling@gmail.com>
2014-01-19 21:55:24 -0800

    notifications work

--- hello_world/hello_world_peripheral.cpp
+++ hello_world/hello_world_peripheral.cpp
@@ -1,4 +1,5 @@
 #include <avr/pgmspace.h>
+#include <EEPROM.h>
 #include "hello_world_peripheral.h"
 #include "dlog.h"
 #include "services.h"
@@ -38,19 +39,15 @@ HelloWorldPeripheral::HelloWorldPeripheral(uint8_t reqn, uint8_t rdyn): BlueCapP
 }
 
 void HelloWorldPeripheral::didReceiveData(uint8_t characteristicId, uint8_t* data, uint8_t length) {
-  // switch(characteristicId) {
-  //   case PIPE_HELLO_WORLD_UPDATE_PERIOD_RX_ACK:
-  //     setUpdatePeriod(data, length);
-  //     break;
-  // }
+  switch(characteristicId) {
+    case PIPE_HELLO_WORLD_UPDATE_PERIOD_RX_ACK:
+      setUpdatePeriod(data, length);
+      break;
+  }
 }
 
 void HelloWorldPeripheral::didReceiveError(uint8_t pipe, uint8_t) {
   DLOG(F("HelloWorldPeripheral"));
-}
-
-bool HelloWorldPeripheral::areAllPipesAvailable() {
-  return isPipeAvailable(PIPE_UART_OVER_BTLE_UART_TX_TX);
 }
 
 void HelloWorldPeripheral::loop() {
@@ -58,28 +55,38 @@ void HelloWorldPeripheral::loop() {
   BlueCapPeripheral::loop();
 }
 
+bool HelloWorldPeripheral::arePipesAvailable() {
+  return isPipeAvailable(PIPE_HELLO_WORLD_GREETING_TX) || isPipeAvailable(PIPE_BATTERY_BATTERY_LEVEL_TX);
+}
+
 void HelloWorldPeripheral::setUpdatePeriod(uint8_t* data, uint8_t size) {
-    // uint16_t bigVal;
-    // memcpy(&bigVal, data, PIPE_HELLO_WORLD_UPDATE_PERIOD_RX_ACK_MAX_SIZE);
-    // updatePeriod = uint16BigToHost(bigVal);
-    // if (updatePeriod > MIN_UPDATE_PERIOD && updatePeriod < MAX_UPDATE_PERIOD) {
-    //   sendAck(PIPE_HELLO_WORLD_UPDATE_PERIOD_RX_ACK);
-    // } else {
-    //   sendNack(PIPE_HELLO_WORLD_UPDATE_PERIOD_RX_ACK, INVALID_UPDATE_PERIOD_ERROR);
-    // }
-    // DLOG(F("Hello World Update Period Update"));
-    // DLOG(updatePeriod, DEC);
+    uint16_t bigVal;
+    memcpy(&bigVal, data, PIPE_HELLO_WORLD_UPDATE_PERIOD_RX_ACK_MAX_SIZE);
+    updatePeriod = uint16BigToHost(bigVal);
+    if (updatePeriod > MIN_UPDATE_PERIOD && updatePeriod < MAX_UPDATE_PERIOD) {
+      sendAck(PIPE_HELLO_WORLD_UPDATE_PERIOD_RX_ACK);
+    } else {
+      sendNack(PIPE_HELLO_WORLD_UPDATE_PERIOD_RX_ACK, INVALID_UPDATE_PERIOD_ERROR);
+    }
+    DLOG(F("Hello World Update Period Update"));
+    DLOG(updatePeriod, DEC);
 }
 
 void HelloWorldPeripheral::setGreeting() {
-  // if (millis() % updatePeriod == 0) {
-  //   DLOG(F("Greeting"));
-  //   char* greeting = greetings[greetingIndex];
-  //   DLOG(greeting);
-  //   sendData(PIPE_HELLO_WORLD_GREETING_TX, (uint8_t*)greeting, strlen(greeting));
-  //   greetingIndex++;
-  //   if (greetingIndex >= GREETING_COUNT) {
-  //     greetingIndex = 0;
-  //   }
-  // }
+  if (millis() % updatePeriod == 0) {
+    DLOG(F("Greeting"));
+    char* greeting = greetings[greetingIndex];
+    DLOG(greeting);
+    sendData(PIPE_HELLO_WORLD_GREETING_TX, (uint8_t*)greeting, strlen(greeting));
+    greetingIndex++;
+    if (greetingIndex >= GREETING_COUNT) {
+      greetingIndex = 0;
+    }
+  }
+}
+
+void HelloWorldPeripheral::writeParams() {
+}
+
+void HelloWorldPeripheral::readParams() {
 }
--- hello_world/hello_world_peripheral.h
+++ hello_world/hello_world_peripheral.h
@@ -8,17 +8,19 @@ public:
 
   HelloWorldPeripheral(uint8_t reqn, uint8_t rdyn);
   void loop();
-  bool areAllPipesAvailable();
 
 protected:
 
   void didReceiveData(uint8_t characteristic_id, uint8_t* data, uint8_t length);
   void didReceiveError(uint8_t pipe, uint8_t errorCode);
+  bool arePipesAvailable();
 
 private:
 
   void setUpdatePeriod(uint8_t* data, uint8_t size);
   void setGreeting();
+  void writeParams();
+  void readParams();
 
 };
 
