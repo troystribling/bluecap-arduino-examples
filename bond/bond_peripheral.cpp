8bfc2b58bcabbb336b5bd9d75c9306ae715675be
Troy Stribling <troy.stribling@gmail.com>
2014-01-26 12:55:29 -0800

    more work on bonding. does not work but did not break unsecure connections

--- bond/bond_peripheral.cpp
+++ bond/bond_peripheral.cpp
@@ -6,7 +6,7 @@
 static services_pipe_type_mapping_t services_pipe_type_mapping[NUMBER_OF_PIPES] = SERVICES_PIPE_TYPE_MAPPING_CONTENT;
 static hal_aci_data_t setup_msgs[NB_SETUP_MESSAGES] PROGMEM = SETUP_MESSAGES_CONTENT;
 
-BondPeripheral::BondPeripheral(uint8_t _reqn, uint8_t _rdyn, uint16_t _eepromOffset): BlueCapPeripheral(_reqn, _rdyn, true, _eepromOffset) {
+BondPeripheral::BondPeripheral(uint8_t _reqn, uint8_t _rdyn, uint16_t _eepromOffset): BlueCapPeripheral(_reqn, _rdyn, _eepromOffset, true) {
   setServicePipeTypeMapping(services_pipe_type_mapping, NUMBER_OF_PIPES);
   setSetUpMessages(setup_msgs, NB_SETUP_MESSAGES);
 }
