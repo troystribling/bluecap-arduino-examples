2acc2582e166ffdde7e104cd765eee6bdb77a1a9
Troy Stribling <troy.stribling@gmail.com>
2014-02-16 11:32:58 -0800

    multiple bond support works for a single bond

--- bond/bond.ino
+++ bond/bond.ino
@@ -9,6 +9,7 @@
 #define RDYN_PIN      8
 #define EEPROM_OFFSET 0
 #define RESET_PIN     6
+#define ADD_BOND_PIN  5
 #define MAX_BONDS     2
 
 BondPeripheral bond(REQN_PIN, RDYN_PIN, EEPROM_OFFSET, MAX_BONDS);
@@ -19,10 +20,9 @@ void setup() {
 #endif
 
   pinMode(RESET_PIN, INPUT);
-  DLOG(F("To delete the bond stored in EEPROM, connect Pin 6 to 3.3v and Reset."));
-  DLOG(F("Make sure that the bond on the phone/PC is deleted as well."));
-  DLOG(F("Reset pin value:"));
-  DLOG(digitalRead(RESET_PIN), DEC);
+  pinMode(ADD_BOND_PIN, INPUT);
+  DLOG(F("To delete the bond stored in EEPROM, connect pin 6 to 5v and reset."));
+  DLOG(F("To add a bond connect pin 5 to 5v."));
   if (digitalRead(RESET_PIN) == HIGH) {
     DLOG(F("Clearing EEPROM bond"));
     bond.clearBondData();
@@ -34,5 +34,10 @@ void setup() {
 
 void loop() {
   bond.loop();
+  if (digitalRead(ADD_BOND_PIN) == HIGH) {
+    bond.addBond();
+    DLOG(F("Added bond. Remove wire"));
+    while(1){delay(1000);};
+  }
 }
 
